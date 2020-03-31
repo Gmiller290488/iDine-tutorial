bc18037b7a0242508ddde99da8bd3c4955db3478
CTingCTer <gmiller290488@gmail.com>
2020-03-31 15:10:12 +0100

    First commit - Has listView and detailView for menu items

---
+++ iDine/ItemRow.swift
@@ -0,0 +1,50 @@
+//
+//  ItemRow.swift
+//  iDine
+//
+//  Created by Gareth Miller on 31/03/2020.
+//  Copyright © 2020 Gareth Miller. All rights reserved.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+
+    static let colors: [String: Color] = [
+        "D": .purple, "G": .black, "N": .red, "S": .blue, "V": .green
+    ]
+    var item: MenuItem
+
+    var body: some View {
+        NavigationLink(destination: ItemDetail(item: item)) {
+            HStack {
+                Image(item.thumbnailImage)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.gray, lineWidth: 2))
+                VStack(alignment: .leading) {
+                    Text(item.name)
+                        .font(.headline)
+                    Text("$\(item.price)")
+                }
+
+                Spacer()
+
+                ForEach(item.restrictions, id: \.self) { restriction in
+                    Text(restriction)
+                        .font(.caption)
+                        .fontWeight(.black)
+                        .padding(5)
+                        .background(Self.colors[restriction, default: .black])
+                        .clipShape(Circle())
+                        .foregroundColor(.white)
+                }
+            }
+        }
+    }
+}
+
+struct ItemRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemRow(item: MenuItem.example)
+    }
+}
