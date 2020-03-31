78e419d735f505a7d284baa2b787e7743717d17b
CTingCTer <gmiller290488@gmail.com>
2020-03-31 18:10:30 +0100

    Added a pickup time picker

--- iDine/OrderView.swift
+++ iDine/OrderView.swift
@@ -9,9 +9,9 @@
 import SwiftUI
 
 struct OrderView: View {
-    
+
     @EnvironmentObject var order: Order
-    
+
     var body: some View {
         NavigationView {
             List {
@@ -22,24 +22,29 @@ struct OrderView: View {
                             Spacer()
                             Text("$\(item.price)")
                         }
-                    }
+                    }.onDelete(perform: deleteItems)
                 }
-                
+
                 Section {
-                    NavigationLink(destination: Text("Check out")) {
+                    NavigationLink(destination: CheckoutView()) {
                         Text("Place order")
-                    }
+                    }.disabled(order.items.isEmpty)
                 }
             }
             .navigationBarTitle("Order")
             .listStyle(GroupedListStyle())
+            .navigationBarItems(trailing: EditButton())
         }
+    }
+
+    func deleteItems(at offsets: IndexSet) {
+        order.items.remove(atOffsets: offsets)
     }
 }
 
 struct OrderView_Previews: PreviewProvider {
     static let order = Order()
-    
+
     static var previews: some View {
         OrderView().environmentObject(order)
     }
